5ef74ac898a51715e408479577499cb0d6e36606
Marceau-h <git@marceau-h.fr>
2025-05-07 18:13:48 +0200

    Add Transfo and S2SMultiHeadAttn models to the module and update __all__ exports

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,40 @@
+from .Language import (
+    Language,
+    RANDOM_STATE,
+    TEST_SIZE,
+    SHUFFLE,
+    SOS_ID,
+    SOS_TOKEN,
+    EOS_ID,
+    EOS_TOKEN,
+    PAD_ID,
+    PAD_TOKEN,
+    read_data,
+    extract_test_data
+)
 from .models import (
-    BaseModel,
     InvalidConfigError,
+    BaseModel,
+    S2SNoAttn,
+    Transfo,
 )
 
 __all__ = [
-    "BaseModel",
     "InvalidConfigError",
+    "BaseModel",
+    "S2SNoAttn",
+    "Transfo",
+
+    "Language",
+    "RANDOM_STATE",
+    "TEST_SIZE",
+    "SHUFFLE",
+    "SOS_ID",
+    "SOS_TOKEN",
+    "EOS_ID",
+    "EOS_TOKEN",
+    "PAD_ID",
+    "PAD_TOKEN",
+    "read_data",
+    "extract_test_data"
 ]
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,9 +1,13 @@
 from .BaseModel import BaseModel, InvalidConfigError
 from .S2SNoAttn import S2SNoAttn
+from .Transfo import Transfo
+from .S2SMultiHeadAttn import S2SMultiHeadAttn
 
 models: dict[str, type[BaseModel]] = {
     "base": BaseModel,
     "no_attn": S2SNoAttn,
+    "transfo": Transfo,
+    "multi_head_attn": S2SMultiHeadAttn,
 }
 
 __all__ = [
@@ -11,4 +15,5 @@ __all__ = [
     "InvalidConfigError",
     "models",
     "S2SNoAttn",
+    "Transfo",
 ]
